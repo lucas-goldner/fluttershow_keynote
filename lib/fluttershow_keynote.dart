dce9dff5a56d1fb2596bfd03d40c42230d953b7b
lucas-goldner <lucas.goldner@googlemail.com>
2023-07-15 00:13:51 +0200

    doc: Add example repo

---
+++ lib/fluttershow_keynote.dart
@@ -0,0 +1,6 @@
+/// FlutterShow Keynote
+library fluttershow_keynote;
+
+export 'package:fluttershow_keynote/model/enum/variants.dart';
+export 'package:fluttershow_keynote/style/keynote_gradients.dart';
+export 'package:fluttershow_keynote/style/keynote_textstyles.dart';
